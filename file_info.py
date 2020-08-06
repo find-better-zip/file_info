3515940ba0c17034228ebeb75324b91fe6451dda
sleepteacher <62042979+sleepteacher@users.noreply.github.com>
2020-08-07 03:23:12 +0900

    Update file_info.py
    오류 수정, 코드 간결화, 주석추가

--- file_info.py
+++ file_info.py
@@ -2,28 +2,28 @@
 import math
 
 def get_byteArr(file_obj):
-    return list(file_obj.read())
+    return list(file_obj.read()) # 파일의 문자열을 배열화 함
 
 def get_fileSize(byteArr):
-    return len(byteArr)
+    return len(byteArr) # 파일의 문자열 길이를 수치화 함
 
 def get_freqList(byteArr, fileSize):
-    freqList = []
+    freqList = []  
     for b in range(256):
         ctr = 0
         for byte in byteArr:
-            if byte == b:
-                ctr += 1
-        freqList.append(float(ctr)/fileSize)
+            if ord(byte) == b:
+                ctr += 1 # 파일 전체를 뒤져서 해당 아스키 코드와 일치하면 ctr의 값을 1씩 추가한다.
+        freqList.append(float(ctr)/fileSize) # 해당 알파벳 갯수/(파일 크기)를 추가
     return freqList
 
 def get_entropy(freqList):
-    ent = 0.0
-    for freq in freqList:
+    ent = 0.0 # 엔트로피 초기화
+    for freq in freqList: 
         if freq > 0:
-            ent = ent + freq * math.log(freq, 2)
-    ent = -ent
+            ent += freq * math.log(freq, 0.5) * freq# freqList 리스트에서 -log_1/2(freq) 수행 후 ent 값을 더해줌
     return ent
+
 
 class all:
     def __init__(self, file_obj):
